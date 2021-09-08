344cb16a8c4a605ba4fb1d086b9c87e26c0449db
owujib <48925577+owujib@users.noreply.github.com>
2021-09-08 10:23:17 +0100

    solved day 4 errors

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express'); //import express
 const path = require('path');
+const mongoose = require('mongoose');
 const homeRoutes = require('./routes/home.routes');
 const postsRoutes = require('./routes/posts.routes');
 //initialize our app
@@ -7,7 +8,7 @@ const app = express();
 
 //post middleware
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({ extended: false }));
 
 //templating set up
 app.set('views', 'ejs');
@@ -26,6 +27,24 @@ app.get('/api/names', (req, res, next) => {
     names: ['Ejike', 'Chinedu', 'Smart'],
   });
 });
+
+app.all('*', (req, res, next) => {
+  res.render('404.ejs', {
+    title: 'Error: 404',
+    data: 'oops page not found',
+  });
+});
+
+mongoose
+  .connect('mongodb://localhost:27017/media', {
+    useNewUrlParser: true,
+  })
+  .then(() => {
+    console.log('database is connected');
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
--- routes/posts.routes.js
+++ routes/posts.routes.js
@@ -1,14 +1,51 @@
 const express = require('express');
+const multer = require('multer');
+const path = require('path');
+
+const storage = multer.diskStorage({
+  destination: function (req, file, callback) {
+    return callback(null, 'uploads/pictures');
+  },
+
+  filename: (req, file, callback) => {
+    const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1e9);
+    const extname = path.extname(file.originalname);
+    return callback(null, file.fieldname + '-' + uniqueSuffix + extname);
+  },
+});
+
+const upload = multer({
+  storage,
+  limits: { fileSize: 100000 },
+  fileFilter: (req, file, callback) => {
+    let fileType = ['image/jpeg', 'image/png'];
+    if (!fileType.includes(file.mimetype)) {
+      return callback(
+        new Error('file type not supported upload png, jpeg or jpg'),
+        false
+      );
+    }
+
+    return callback(null, true);
+  },
+});
 
 const router = express.Router();
 
 router.get('/', (req, res, next) => {
-  console.log(req.query);
-  res.send('hey my post');
+  res.render('posts/list.ejs', {
+    title: 'Posts',
+  });
 });
-router.get('/:name', (req, res, next) => {
-  console.log(req.params);
-  res.send('hey my post');
+
+router.get('/create', (req, res, next) => {
+  res.render('posts/create.ejs', {
+    title: 'create post',
+  });
+});
+
+router.post('/create', upload.single('image'), (req, res, next) => {
+  res.send(req.file);
 });
 
 module.exports = router;
